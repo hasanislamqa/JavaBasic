1fd7ebbe368e6aa1ff5f5161f076596b1af1652f
hasanurislam <hasanislamqa@gmail.com>
2020-01-04 10:36:59 -0500

    IfElseStatement2

---
+++ src/IfElseStatement2.java
@@ -0,0 +1,18 @@
+public class IfElseStatement2 {
+    static int banana=60;
+    static int apple=25;
+    public static void produce(){
+      if (banana>80){
+          System.out.println(banana);
+      }else{
+
+
+          System.out.println(apple);
+
+
+
+        }
+    }
+
+
+}
